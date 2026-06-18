0f5f5abac33514320cafe07d1625c4fee1940ed4
Sunny Tseng <sunnyyctseng@gmail.com>
2026-06-17 23:04:29 -0700

    extracted audio and datetime

--- R/01_TAIGA_initial_data_arrangement.R
+++ R/01_TAIGA_initial_data_arrangement.R
@@ -6,45 +6,59 @@
 #data wrangline
 library(tidyverse)
 library(here)
+library(janitor)
 
 #audio processing
 library(av)
+library(tidymedia)
 library(birdnetTools)
 
 
 
-# extract audio from video ------------------------------------------------
+# extract audio, datetime from video ------------------------------------------------
 
 
-video_folder <- "D:/2026_eastern_grassowl_Taiwan/土庫北A89"
-output_folder <- "D:/2026_eastern_grassowl_Taiwan/土庫北A89_audio"
+video_folder <- "D:/2026_eastern_grassowl_Taiwan/順揚有利172"
+output_folder <- "D:/2026_eastern_grassowl_Taiwan/順揚有利172_audio"
+output_file <- tibble()
 
-mp4_files <- list.files(path = video_folder, pattern = "\\.mp4$", full.names = TRUE, ignore.case = TRUE)
+mp4_files <- list.files(path = video_folder,
+                        pattern = "\\.mp4$",
+                        full.names = TRUE,
+                        ignore.case = TRUE,
+                        recursive = TRUE)
+
 
 for (video_path in mp4_files) {
 
-  # Extract the base file name (e.g., "recording1.mp4" -> "recording1")
+  # 1. extract audio
+  # get the base file name (e.g., "recording1.mp4" -> "recording1")
   file_name <- tools::file_path_sans_ext(basename(video_path))
 
-  # Construct the new output path with a .wav extension
+  # construct the new output path with a .wav extension
   wav_path <- file.path(output_folder, paste0(file_name, ".wav"))
-
   cat("Converting:", basename(video_path), "-->", paste0(file_name, ".wav\n"))
 
-  # Run the conversion
-  av_audio_convert(video_path,
-                   wav_path,
-                   channels = 1,
-                   sample_rate = 44100)
+  # run the conversion
+  av_audio_convert(video_path, wav_path)
+
+
+  # 2. extract date and time
+  video_info <- mediainfo_query(file = video_path,
+                                section = "General",
+                                parameters = c("Encoded_Date", "Duration", "FileSize"))
+  audio_info <- mediainfo_query(file = wav_path,
+                                section = "Audio",
+                                parameters = c("SamplingRate", "Channels", "BitDepth", "Format"))
+  file_info <- c(video_info, audio_info)
+
+  output_file <- bind_rows(output_file, file_info)
 }
 
-cat("\nAll files successfully converted and saved to:", output_folder, "\n")
+av_file_metadata <- output_file %>%
+  clean_names() %>%
+  rename(datetime = encoded_date,
+         filepath_video = file_1,
+         filepath_audio = file_5)
 
 
-# Run the extraction
-av_audio_convert(video = video_file,
-  output = audio_output,
-  channels = 1,          # Converts stereo to mono (highly recommended for BirdNET)
-  sample_rate = 44100)
-
-message("Audio extraction complete!")
