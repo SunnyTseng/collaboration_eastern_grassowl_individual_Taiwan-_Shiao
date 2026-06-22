6f9a781df2f497b746281288216f44ef97da1c0e
Sunny Tseng <sunnyyctseng@gmail.com>
2026-06-22 12:59:01 -0700

    updated audio folder structure and the function

--- R/01_TAIGA_initial_data_arrangement.R
+++ R/01_TAIGA_initial_data_arrangement.R
@@ -15,7 +15,6 @@ library(janitor)
 #audio processing
 library(av)
 library(tuneR)
-library(bioacoustics)
 library(ohun)
 library(tidymedia)
 
@@ -29,7 +28,7 @@ library(birdnetTools)
 extract_audio_metadata <- function(video_folder, extraction = TRUE) {
 
   # create empty folder for extracted audio
-  audio_folder <- paste0(video_folder, "_audio")
+  audio_folder <- sub("video", "audio", video_folder)
 
   if (!dir.exists(audio_folder)) {
     dir.create(audio_folder, recursive = TRUE)
@@ -48,28 +47,32 @@ extract_audio_metadata <- function(video_folder, extraction = TRUE) {
 
 
   # The main loop to process each video file
-  for (video_path in video_files) {
+  for (video_file in video_files) {
 
     # 1. extract audio
-    # get the base file name (e.g., "recording1.mp4" -> "recording1")
-    file_name <- tools::file_path_sans_ext(basename(video_path))
+    audio_file <- video_file %>%
+      str_replace("TAIGA_video", "TAIGA_audio") %>%
+      str_replace("\\.[^.]+$", ".wav")
 
-    # construct the new output path with a .wav extension
-    audio_path <- file.path(audio_folder, paste0(file_name, ".wav"))
+    target_dir <- dirname(audio_file)
+
+    if (!dir.exists(target_dir)) {
+      dir.create(target_dir, recursive = TRUE)
+    }
 
     # run the conversion
     if(extraction == TRUE){
-      av_audio_convert(video_path, audio_path)
+      av_audio_convert(video_file, audio_file)
     } else {
-      cat("Audio extraction skipped for:", basename(video_path), "\n")
+      cat("Audio extraction skipped for:", basename(video_file), "\n")
     }
 
 
     # 2. extract date and time and other metadata
-    video_info <- mediainfo_query(file = video_path,
+    video_info <- mediainfo_query(file = video_file,
                                   section = "General",
                                   parameters = c("Encoded_Date", "Duration", "FileSize"))
-    audio_info <- mediainfo_query(file = audio_path,
+    audio_info <- mediainfo_query(file = audio_file,
                                   section = "Audio",
                                   parameters = c("SamplingRate", "Channels", "BitDepth", "Format"))
     file_info <- c(video_info, audio_info)
@@ -85,8 +88,8 @@ extract_audio_metadata <- function(video_folder, extraction = TRUE) {
            filepath_video = file_1,
            filepath_audio = file_5) %>%
     mutate(site = str_split_i(filepath_video, "/", -2) %>% str_extract("\\p{Han}+"),
-           owl_id = str_split_i(filepath_video, "/", 3) %>% str_extract("[A-Za-z0-9]+")) %>%
-    select(site, owl_id, datetime, sampling_rate, channels, bit_depth, format,
+           owl_id = str_split_i(filepath_video, "/", 4) %>% str_extract("[A-Za-z0-9]+")) %>%
+    select(owl_id, site, datetime, sampling_rate, channels, bit_depth, format,
            filepath_video, filepath_audio)
 
   return(av_file_metadata)
@@ -94,15 +97,12 @@ extract_audio_metadata <- function(video_folder, extraction = TRUE) {
 
 # extract audio, datetime from video ------------------------------------------------
 
-# include all the file except the one with "audio" in the name
-
-folder_list <- list.dirs(path = "D:/2026_eastern_grassowl_Taiwan",
+folder_list <- list.dirs(path = "D:/2026_eastern_grassowl_Taiwan/TAIGA_video",
                          full.names = TRUE,
                          recursive = FALSE)
 
 metadata_all <- tibble()
 for (folder in folder_list) {
-  cat("Processing folder:", folder, "\n")
   metadata <- extract_audio_metadata(folder, extraction = TRUE)
   metadata_all <- bind_rows(metadata_all, metadata)
 }
